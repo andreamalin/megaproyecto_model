3b603e9c351a2baf159dd3d66bb3090f6dce08d7
Brandon Hernández <brandonhernandez_10@outlook.es>
2023-10-25 23:37:07 -0600

    feat: finish predict part

--- server.py
+++ server.py
@@ -23,41 +23,40 @@ async def predictModel(resource: Resource):
 
     if (resource.isVideo):
       if (resource.langId == '651b5d410ae23c0573d7953e'):
-        result = process_video(video_path, id)
+        result = process_video(video_path, id)[0]
       else:
-        result = process_video_asl(video_path, id)
+        result = process_video_asl(video_path, id)[0]
     else:
       storage_client = storage.Client()
       bucket = storage_client.bucket(CDN_BUCKET_NAME)
       blobs = bucket.list_blobs(prefix=f'{resource.langId}/{resource.key}/')
       image_names = [blob.name for blob in blobs]
 
-      result = []
+      letters = []
       for index, image_name in enumerate(image_names):
-        print(f'{CDN_BUCKET}{image_name}', id, index + 1)
-        result.append(
+        letters.append(
           process_image(
             f'{CDN_BUCKET}{image_name}',
             id,
             index + 1,
           )[0]
         )
-      print(result)
+      result = [''.join(letters)]
 
-    # # Construct payload for PUT request
-    # data = {
-    #   "_id": id,
-    #   "options": result[0],
-    #   "apiPassword": '7AuY-YmfBv-624MKj2sQ',
-    #   "apiUsername": 'W4B.n-sdsh',
-    # }
+    # Construct payload for PUT request
+    data = {
+      "_id": id,
+      "options": result,
+      "apiPassword": '7AuY-YmfBv-624MKj2sQ',
+      "apiUsername": 'W4B.n-sdsh',
+    }
 
-    # # Send PUT request to BACKEND_API
-    # async with httpx.AsyncClient() as client:
-    #   response = await client.put(f"{BACKEND_API}/resources/model", json=data)
+    # Send PUT request to BACKEND_API
+    async with httpx.AsyncClient() as client:
+      response = await client.put(f"{BACKEND_API}/resources/model", json=data)
     
-    # # You can handle the response as required, for now, I'll just return it
-    # return response.json()
+    # You can handle the response as required, for now, I'll just return it
+    return response.json()
   except Exception as e:
     print(e)
     # Return error details for easier debugging
